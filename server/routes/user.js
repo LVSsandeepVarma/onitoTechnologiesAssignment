c342677f4a5a35e8140c8ec16e72bfaacf8e3a9a
LVSsandeepVarma <sandeeplakham@gmail.com>
2023-04-25 20:44:20 +0530

    added database schema and tested with postman with dummy data

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,10 +3,32 @@ const mongoose = require("mongoose")
 const router = express.Router()
 const jwt = require("jsonwebtoken")
 const bcrypt = require("bcrypt")
+const testModel = require("../model/testModel")
+const userModel = require("../model/userModel")
 
 
 router.get("/",(req,res)=>{
     return res.status(200).send({data:"successfully routed!!"})
 })
 
+router.post("/register", (req,res)=>{
+    console.log(req.body)
+    if(req.body.name != "" && req.body.DOB !="" && req.body.gender !=""){
+        const userData = {...req.body}
+        console.log(userData)
+        userModel.create(userData)
+        res.status(200).send({data:"stored successfully"})
+    }
+    else{
+        res.status(400).send({data: "please fill all the mandatory fields"})
+    }
+})
+
+router.post("/test",async (req,res)=>{
+    console.log(req.body)
+    await testModel.create({
+        name: req.body.name
+    })
+    res.status(200).send("successful")
+})
 module.exports = router
